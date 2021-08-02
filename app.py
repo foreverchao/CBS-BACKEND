fcb419b95813a5cb96bdd735d8d17c085c97ea8f
foreverchao <49773012+foreverchao@users.noreply.github.com>
2021-08-02 16:51:27 +0800

    user api test

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,jsonify,request
+from mongo_practice import getAllUserInfo,getOneUserInfo,delOneUser,addOneUser
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+import pymongo
+from werkzeug.security import generate_password_hash,check_password_hash
 
+
+app = Flask(__name__)
+app.secret_key = "secreatkey"
+app.config['MONGO_URI'] = ""
 @app.route("/")
 def home():
     return "Hello Flask HI HI"
 
-@app.route("/test")
-def test():
-    return "Hello Flask test"
+
+@app.route("/add",methods=["POST"])
+def add_user():
+    _json = request.json
+    _email = _json['email']
+    _password = _json['password']
+    #if _email and _password and request.method == 'POST':
+    resp = addOneUser(_email,_password)
+    return resp
+    
+@app.route("/users")
+def users():
+    users = getAllUserInfo()
+    #print(data)
+    return users
+
+
+@app.route("/user/<id>")
+def user(id):
+    user = getOneUserInfo(id)
+    return user
+
+@app.route("/delete/<id>",methods=['DELETE'])
+def delete_user(id):
+    userDelete = delOneUser(id)
+    return userDelete
 
 if __name__ == "__main__":
     app.run()
---
+++ mongo_practice.py
@@ -0,0 +1,36 @@
+import collections
+from flask.json import jsonify
+import pymongo
+from bson.objectid import ObjectId
+from pymongo import collection
+from bson.json_util import dumps
+
+client = pymongo.MongoClient("mongodb+srv://root:root123@mycluster.bqv3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.website
+collection = db.members
+
+
+def addOneUser(_email,_password):
+    user = collection.insert_one({
+        'email':_email,
+        'password':_password
+    })
+    resp = jsonify("user add success")
+    return resp
+
+def getAllUserInfo():
+    users = collection.find()
+    resp = dumps(users)
+    #print (doc)
+        #print (userdata[0])
+    return resp
+
+def getOneUserInfo(id):
+    user = collection.find_one({'_id' :ObjectId(id)})
+    resp = dumps(user)
+    return resp
+
+def delOneUser(id):
+    collection.delete_one({'_id' :ObjectId(id)})
+    resp = jsonify("user delete success")
+    return resp
