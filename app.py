f18e3fa6c46ca14fc15733f683aa686bd5da83cf
foreverchao <49773012+foreverchao@users.noreply.github.com>
2021-08-15 13:52:37 +0800

    jwt test

--- app.py
+++ app.py
@@ -1,19 +1,38 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from mongo_practice import getAllUserInfo,getOneUserInfo,delOneUser,addOneUser
 from bson.json_util import dumps
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+
+
 
 
 app = Flask(__name__)
 app.secret_key = "secreatkey"
 app.config['MONGO_URI'] = ""
 CORS(app)
+jwt = JWTManager()
+app.config['JWT_SECRET_KEY'] = 'this-should-be-change'
+jwt.init_app(app)
 #def home():
     #return "Hello Flask health ok"
 
 @app.route("/")
 def index():
     return 'hellth ok'
+
+@app.route('/login', methods=['POST'])
+def login(): 
+    username = request.json.get('username', None) 
+    password = request.json.get('password', None) 
+
+    if username != 'test' or password != 'test': 
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
     
 @app.route("/add",methods=["POST"])
 def add_user():
@@ -25,6 +44,7 @@ def add_user():
     return resp
     
 @app.route("/users")
+@jwt_required()
 def users():
     users = getAllUserInfo()
     #print(data)
